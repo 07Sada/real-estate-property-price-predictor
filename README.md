1b17e4c4c2de0e26ab217903448ef1cfe47327b7
07Sada <sadashiv.nandanikar07@gmail.com>
2024-02-16 20:58:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+real-estate-property-price-predictor
+==============================
+
+This project help to predict the real estate property's price in Guragram Area.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project help to predict the real estate property's price in Guragram Area.',
+    author='Sada',
+    license='',
+)
